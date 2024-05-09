a4124da4f9b61bf950888c60d95293824209c29b
Sanusi Hassan <sanusihassan122@gmail.com>
2024-05-09 03:29:19 +0200

    added split-pdf

--- unzip_and_move.py
+++ unzip_and_move.py
@@ -66,5 +66,5 @@ def move_png_files(zip_folder_name):
             print(f"Error: Failed to extract {zip_folder_name}")
 
 # Example usage:
-zip_folder_name = "remove-pages.zip"
+zip_folder_name = "split-pdf.zip"
 move_png_files(zip_folder_name)
