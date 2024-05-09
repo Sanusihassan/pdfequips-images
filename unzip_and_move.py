17c5b1f2f3dc8c2e5a68d08bd630b231fc9d9d3b
Sanusi Hassan <sanusihassan122@gmail.com>
2024-05-09 21:15:31 +0200

    added add-watermark

--- unzip_and_move.py
+++ unzip_and_move.py
@@ -66,5 +66,5 @@ def move_png_files(zip_folder_name):
             print(f"Error: Failed to extract {zip_folder_name}")
 
 # Example usage:
-zip_folder_name = "merge-pdf.zip"
+zip_folder_name = "add-watermark.zip"
 move_png_files(zip_folder_name)
