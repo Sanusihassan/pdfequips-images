9ecd2721da29feee54f38632ecb008039ada8985
Sanusi Hassan <sanusihassan122@gmail.com>
2024-05-10 16:00:11 +0200

    added lock/unlock/rotate-pdf

--- unzip_and_move.py
+++ unzip_and_move.py
@@ -66,5 +66,5 @@ def move_png_files(zip_folder_name):
             print(f"Error: Failed to extract {zip_folder_name}")
 
 # Example usage:
-zip_folder_name = "number-pdf.zip"
+zip_folder_name = "unlock-pdf.zip"
 move_png_files(zip_folder_name)
