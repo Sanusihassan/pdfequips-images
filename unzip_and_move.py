1bd528f5f7f89141b2960bca59324e3aa3cf8da2
Sanusi Hassan <sanusihassan122@gmail.com>
2024-06-25 23:22:23 +0200

    added organize-pdf

--- unzip_and_move.py
+++ unzip_and_move.py
@@ -66,5 +66,5 @@ def move_png_files(zip_folder_name):
             print(f"Error: Failed to extract {zip_folder_name}")
 
 # Example usage:
-zip_folder_name = "text-to-pdf.zip"
+zip_folder_name = "organize-pdf.zip"
 move_png_files(zip_folder_name)
